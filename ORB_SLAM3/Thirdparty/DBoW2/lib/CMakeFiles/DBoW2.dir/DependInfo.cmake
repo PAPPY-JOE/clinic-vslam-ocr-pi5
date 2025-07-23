6885ddd4d4cfa326143b154b725f6b6cb7c0dbf9
PAPPY-JOE <fatoyejoseph@gmail.com>
2025-07-23 18:20:44 +0100

    Initial commit: Visual SLAM + OCR project on Raspberry Pi 5

---
+++ ORB_SLAM3/Thirdparty/DBoW2/lib/CMakeFiles/DBoW2.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspace/orbslam3_ws/Thirdparty/DBoW2/DBoW2/BowVector.cpp" "/workspace/orbslam3_ws/Thirdparty/DBoW2/lib/CMakeFiles/DBoW2.dir/DBoW2/BowVector.cpp.o"
+  "/workspace/orbslam3_ws/Thirdparty/DBoW2/DBoW2/FORB.cpp" "/workspace/orbslam3_ws/Thirdparty/DBoW2/lib/CMakeFiles/DBoW2.dir/DBoW2/FORB.cpp.o"
+  "/workspace/orbslam3_ws/Thirdparty/DBoW2/DBoW2/FeatureVector.cpp" "/workspace/orbslam3_ws/Thirdparty/DBoW2/lib/CMakeFiles/DBoW2.dir/DBoW2/FeatureVector.cpp.o"
+  "/workspace/orbslam3_ws/Thirdparty/DBoW2/DBoW2/ScoringObject.cpp" "/workspace/orbslam3_ws/Thirdparty/DBoW2/lib/CMakeFiles/DBoW2.dir/DBoW2/ScoringObject.cpp.o"
+  "/workspace/orbslam3_ws/Thirdparty/DBoW2/DUtils/Random.cpp" "/workspace/orbslam3_ws/Thirdparty/DBoW2/lib/CMakeFiles/DBoW2.dir/DUtils/Random.cpp.o"
+  "/workspace/orbslam3_ws/Thirdparty/DBoW2/DUtils/Timestamp.cpp" "/workspace/orbslam3_ws/Thirdparty/DBoW2/lib/CMakeFiles/DBoW2.dir/DUtils/Timestamp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DBoW2_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
